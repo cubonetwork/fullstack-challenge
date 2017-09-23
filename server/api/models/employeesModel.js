512cef673c367c480e7e5c03feb7103f4f908bd8
Angelo Santos <angelosds@gmail.com>
2017-09-23 16:58:15 -0300

    Adjusts in employee model

--- server/api/models/employeesModel.js
+++ server/api/models/employeesModel.js
@@ -6,14 +6,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema
 
 var EmployeeSchema = new Schema({
-    Id: Number,
-    Name: String,
-    LastName: String,
-    Particpation: Number,
-    CreateDate: {
+    name: String,
+    lastName: String,
+    participation: Number,
+    createDate: {
         type: Date,
         default: Date.now
     }
-})
+});
 
 module.exports = mongoose.model('Employee', EmployeeSchema);
