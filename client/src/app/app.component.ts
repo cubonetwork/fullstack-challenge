c8375e2a3aa24cb325f5b050224b79d013ebc8e8
Angelo Santos <angelosds@gmail.com>
2017-09-27 22:35:39 -0300

    Added basic style

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.styl'],
+    encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
 }
